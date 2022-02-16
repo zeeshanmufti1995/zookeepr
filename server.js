d4f1abd66c8dabc938703cfc19b426eea9bb25d8
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-02-16 10:08:22 -0600

    Add get method

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
@@ -95,6 +97,23 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
